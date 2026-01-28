9de3a865ac5e230b9a7eb9623536d75ee26aaf7d
gelas78 <128419097+gelas78@users.noreply.github.com>
2026-01-28 13:27:47 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildVoiceStates
+  ]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Bot conectado como ${client.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
